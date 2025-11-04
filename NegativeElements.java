b7cd1c5c1fd4d3dc3d33277d4c711c8aedadc036
Onkar Ganjure <161829340+OnkarGanjure2003@users.noreply.github.com>
2025-11-04 15:32:50 +0530

    Add files via upload

---
+++ NegativeElements.java
@@ -0,0 +1,21 @@
+//code to print negative array elements
+package Lecture6;
+import java.util.Scanner;
+public class NegativeElements {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.print("Enter Array Elements: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Negative Elements Of Array: ");
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0) {
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
