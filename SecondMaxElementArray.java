03afeeedfedea9e1e225adddf5ffa657ee0dd1cf
Onkar Ganjure <161829340+OnkarGanjure2003@users.noreply.github.com>
2025-11-06 19:35:01 +0530

    Add files via upload

---
+++ SecondMaxElementArray.java
@@ -0,0 +1,15 @@
+package Lecture6;
+import java.util.Scanner;
+public class SecondMaxElementArray {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.print("Enter Array Elements: ");
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+    }
+
+}
