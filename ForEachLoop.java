dc28b5017c88f9b7eb465e4c3b3d1dcc57b6e3b7
Onkar Ganjure <161829340+OnkarGanjure2003@users.noreply.github.com>
2025-11-08 23:23:10 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,9 @@
+package Lecture6;
+public class ForEachLoop {
+    public static void main(String[] args) {
+        int[] arr={4,3,2,1,8,9};
+        for(int ele:arr){
+            System.out.print(ele+" ");
+        }
+    }
+}
