252c82b06469531c58c708e69b032e37d320fc70
Onkar Ganjure <161829340+OnkarGanjure2003@users.noreply.github.com>
2025-11-08 23:23:50 +0530

    Add files via upload

--- SecondMaxElementArray.java
+++ SecondMaxElementArray.java
@@ -10,6 +10,20 @@ public class SecondMaxElementArray {
         for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
+        int max=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("First Max Element: "+max);
+        int secmax=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>secmax && arr[i]!=max){
+                secmax=arr[i];
+            }
+        }
+        System.out.println("Second Max element: "+secmax);
     }
 
 }
